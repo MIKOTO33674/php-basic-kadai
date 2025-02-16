01817a7d985b5e6ffea048f115ada59204be7e72
MIKOTO33674 <area_625_0219@yahoo.co.jp>
2025-02-16 16:07:10 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+     function sort_2way($array, $order) {
+      if ($order) {
+        sort($array);
+      } else {
+        rsort($array);
+      }
+      // ソートされた配列を返す
+      return $array;
+    }
+
+    $num = [15, 4, 18, 23, 10];
+
+    // 昇順の結果を受け取って表示
+    $sorted_asc = sort_2way($num, TRUE);
+    echo "昇順にソートされました：<br>";
+    foreach ($sorted_asc as $value) {
+      echo $value . '<br>';
+    }
+
+    // 降順の結果を受け取って表示
+    $sorted_desc = sort_2way($num, FALSE);
+    echo "降順にソートされました：<br>";
+    foreach ($sorted_desc as $value) {
+      echo $value . '<br>';
+    }
+    ?>
+  </p>
+</body>
+
+</html>
